5bab80e212c25e73b7fe23d16dfff8e8dc7e489a
jinen-rathore <jinen.rathore2020@vitstudent.ac.in>
2022-01-23 20:19:25 +0530

    Revert "Revert "first commit""
    This reverts commit 3cad900f5a791727c1d843e76f0db219912610fc.

--- src/screens/Mainscreen/index.js
+++ src/screens/Mainscreen/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import TopView from '../../components/TopView';
-import BottomView from '../../components/BottomView';
 import Login from '../../components/Login';
 
 export default function Mainscreen(){
