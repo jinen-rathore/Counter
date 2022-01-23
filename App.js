3cad900f5a791727c1d843e76f0db219912610fc
jinen-rathore <jinen.rathore2020@vitstudent.ac.in>
2022-01-23 20:18:25 +0530

    Revert "first commit"
    This reverts commit b9844bb276edefc4e75a9be5e73e05228c52daea.

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Mainscreen from './src/screens/Mainscreen';
-import Counter from './src/Counter';
 
 export default function App() {
   return (
     <>
-      <Counter />
+    <Mainscreen />
     </>
   );
 }
