b9844bb276edefc4e75a9be5e73e05228c52daea
jinen-rathore <jinen.rathore2020@vitstudent.ac.in>
2022-01-23 20:13:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Mainscreen from './src/screens/Mainscreen';
+import Counter from './src/Counter';
 
 export default function App() {
   return (
     <>
-    <Mainscreen />
+      <Counter />
     </>
   );
 }
